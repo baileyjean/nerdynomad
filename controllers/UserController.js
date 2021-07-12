0cbb5cc33cc4858d1216eb306c4fbcdad4f60391
Bailey Jean <79871279+baileyjean@users.noreply.github.com>
2021-07-12 15:23:11 -0700

    Add User controller and router

---
+++ controllers/UserController.js
@@ -0,0 +1,66 @@
+const { User } = require('../models')
+
+const GetAllUsers = async (req, res) => {
+  try {
+    let userlist = await User.findAll()
+    res.send(userlist)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetUserById = async (req, res) => {
+  try {
+    const { payload } = res.locals
+    let userId = parseInt(payload.id)
+    console.log(userId)
+    let userFound = await User.findByPk(userId)
+    res.send(userFound)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetUserByEmail = async (req, res) => {
+  try {
+    let userEmail = req.params.email
+    let userByEmail = await User.findAll({
+      where: { email: userEmail },
+      returning: true
+    })
+    res.send(userByEmail)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateUser = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.user_id)
+    let updatedUser = await User.update(req.body, {
+      where: { id: userId },
+      returning: true
+    })
+    res.send(updatedUser)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteUser = async (req, res) => {
+  try {
+    let userId = parseInt(req.params.user_id)
+    await User.destroy({ where: { id: userId } })
+    res.send({ message: `Goodbye, Nerdy Nomad` })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetAllUsers,
+  GetUserById,
+  GetUserByEmail,
+  UpdateUser,
+  DeleteUser
+}
