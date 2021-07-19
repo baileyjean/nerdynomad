59474a5e62527f37cb81c1bb2e15701a763abdf2
Bailey Jean <79871279+baileyjean@users.noreply.github.com>
2021-07-18 20:56:28 -0700

    Add styling to nav bar. Its been 84 years....

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import '../styles/App.css'
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from '../styles/images/logo.png'
 
@@ -7,27 +7,45 @@ const NavBar = (props) => {
   //////////////////////// STATE ////////////////////////
   const { loggedIn, logOut, userID } = props
 
+  const openNav = () => {
+    document.getElementById("side-nav").style.width = "250px";
+    document.getElementById("open-nav").style.marginLeft = "250px";
+  }
+
+  const closeNav = () => {
+    document.getElementById("side-nav").style.width = "0";
+    document.getElementById("open-nav").style.marginLeft = "0";
+  }
+
   //////////////////////// FRONT-END RETURN ////////////////////////
   return loggedIn ? (
-    <header className="nav">
-      <img src={logo} alt={"Nerdy Nomad Logo"} />
-      <NavLink to={"/"}>Home</NavLink>
-      <nav>
-        <NavLink to="/browse-all">Browse Science Centers</NavLink>
-        <NavLink to={`/post-science-center/${userID}`}>Add a Science Center</NavLink>
-        <NavLink to={`/user/${userID}`}>Your Profile </NavLink>
-        <button id="logout" onClick={logOut}> Logout </button>
-      </nav>
-    </header>
+    <div>
+      <span id="open-nav" style={{ cursor: "pointer" }} onClick={openNav}>&#9776;</span>
+      <div id="side-nav" class="nav">
+        <button class="close-nav" onClick={closeNav}>x</button>
+        <img src={logo} alt={"Nerdy Nomad Logo"} />
+        <nav>
+          <span><NavLink style={{ textDecoration: 'none', color: 'white' }} to={"/"}>Home</NavLink></span>
+          <span><NavLink style={{ textDecoration: 'none', color: 'white' }} to="/browse-all">Browse Science Centers</NavLink></span>
+          <span><NavLink style={{ textDecoration: 'none', color: 'white' }} to={`/post-science-center/${userID}`}>Add a Science Center</NavLink></span>
+          <span><NavLink style={{ textDecoration: 'none', color: 'white' }} to={`/user/${userID}`}>Your Profile </NavLink></span>
+          <span><button id="logout" onClick={logOut}> Logout </button></span>
+        </nav>
+      </div>
+    </div>
   ) : (
-    <header className="nav">
-      <img src={logo} alt={"Nerdy Nomad Logo"} />
-      <NavLink to={"/"}>Home</NavLink>
-      <nav>
-        <NavLink to="/browse-all">Browse Science Centers</NavLink>
-        <NavLink to="/oneofus">Login/Signup</NavLink>
-      </nav>
-    </header>
+    <div>
+      <span id="open-nav" style={{ cursor: "pointer" }} onClick={openNav}>&#9776;</span>
+      <div id="side-nav" class="nav">
+        <button class="close-nav" onClick={closeNav}>x</button>
+        <img src={logo} alt={"Nerdy Nomad Logo"} style={{ width: '100%' }} />
+        <nav>
+          <span><NavLink style={{ textDecoration: 'none', color: 'white' }} to={"/"}>Home</NavLink></span>
+          <span><NavLink style={{ textDecoration: 'none', color: 'white' }} to="/browse-all">Browse Science Centers</NavLink></span>
+          <span><NavLink style={{ textDecoration: 'none', color: 'white' }} to="/oneofus">Login/Signup</NavLink></span>
+        </nav>
+      </div>
+    </div>
   )
 }
 export default NavBar;
